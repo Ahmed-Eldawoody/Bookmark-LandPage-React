24a3aaa9893a8d9c3f774c5677085a8dd9f298e1
Ahmed-Eldawoody <60572865+Ahmed-Eldawoody@users.noreply.github.com>
2020-10-14 15:22:21 +0200

    scroll to top z-index issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,7 +23,8 @@ const useStyles = makeStyles((theme) => ({
   root: {
     position: "fixed",
     bottom: theme.spacing(2),
-    right: theme.spacing(2)
+    right: theme.spacing(2),
+    zIndex: 3
   },
   x: {
     color: "black"
